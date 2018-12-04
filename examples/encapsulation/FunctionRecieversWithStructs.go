5293c3827c969498f6f436fc33dca385b71b39a7
Sunit Chatterjee <34455433+chatterjeesunit@users.noreply.github.com>
2018-12-04 17:15:51 +0530

    changed pointer reciever example

--- examples/encapsulation/FunctionRecieversWithStructs.go
+++ examples/encapsulation/FunctionRecieversWithStructs.go
@@ -7,15 +7,19 @@ import (
 
 func main() {
 
-	c1 := Circle{5, Point{5, 4}, "red"}
-	c2 := Circle{5, Point{-3, 4}, "yellow"}
+	c1 := Circle{5, Point{5, 4}, "Red"}
+	c2 := Circle{6, Point{-3, -5}, "Yellow"}
 
-	fmt.Printf("Red Color = %s, Area = %f, Circumference = %f \n", c1.color, c1.Area(), c1.Circumference())
-	fmt.Printf("Yellow Color = %s, Area = %f, Circumference = %f \n", c2.color, c2.Area(), c2.Circumference())
+	fmt.Printf("%s Circle: Area = %f, Circumference = %f \n\n", c1.color, c1.Area(), c1.Circumference())
+	fmt.Printf("%s Circle: Area = %f, Circumference = %f \n\n", c2.color, c2.Area(), c2.Circumference())
 
+	fmt.Printf("Red Circle Overlaps Yellow Circle : %v\n\n", c1.Overlaps(c2))
 
-	fmt.Printf("Red Circle Overlaps Yellow Circle : %v", c1.Overlaps(c2))
-	
+	fmt.Println("Now Increasing radius of Red circle")
+	c1.ChangeRadius(10)
+
+	fmt.Printf("Updated %s Circle: Area = %f, Circumference = %f \n\n", c1.color, c1.Area(), c1.Circumference())
+	fmt.Printf("Red Circle Overlaps Yellow Circle : %v\n", c1.Overlaps(c2))
 }
 
 type Point struct {
@@ -26,27 +30,30 @@ type Point struct {
 type Circle struct {
 	radius int
 	center Point
-	color string
+	color  string
 }
 
-
-func (p *Point) distanceTo(o Point) float64 {
+func (p Point) distanceTo(o Point) float64 {
 	return math.Sqrt(
-			math.Pow(float64(o.x - p.x), 2) +
-			math.Pow(float64(o.y - p.y), 2))
+		math.Pow(float64(o.x-p.x), 2) +
+			math.Pow(float64(o.y-p.y), 2))
 }
 
-func (c *Circle) Area() float64 {
+func (c Circle) Area() float64 {
 	return math.Pow(float64(c.radius), 2) * math.Pi
 }
 
-func (c *Circle) Circumference() float64 {
+func (c Circle) Circumference() float64 {
 	return 2 * math.Pi * float64(c.radius)
 }
 
-func (this *Circle) Overlaps(other Circle) bool {
+func (this Circle) Overlaps(other Circle) bool {
 
 	distanceBetweenCenters := other.center.distanceTo(this.center)
 
-	return distanceBetweenCenters < float64(this.radius + other.radius)
+	return distanceBetweenCenters < float64(this.radius+other.radius)
+}
+
+func (this *Circle) ChangeRadius(radius int) {
+	this.radius = radius
 }
